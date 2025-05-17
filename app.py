14748583d973ad4fe45a4cb9ef834ce994eee804
Shreyash Patukale <shreyashpatukale@gmail.com>
2025-05-18 03:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'cc_users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)  # Increased to 256
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
