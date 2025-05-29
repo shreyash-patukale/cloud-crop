8a49db251d64ef9bc429db96721beac2ddf66b68
Shreyash Patukale <shreyashpatukale@gmail.com>
2025-05-29 22:39:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import os
 import datetime
 import time
@@ -369,4 +368,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
-```
